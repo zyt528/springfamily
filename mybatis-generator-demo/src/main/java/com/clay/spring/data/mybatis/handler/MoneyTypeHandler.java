f56305d9f2f64bf36c8afee94a6fa56be1014c31
zyt528 <zyt528@outlook.com>
2019-09-21 19:17:14 +0800

    optimize

--- mybatis-generator-demo/src/main/java/com/clay/spring/data/mybatis/handler/MoneyTypeHandler.java
+++ mybatis-generator-demo/src/main/java/com/clay/spring/data/mybatis/handler/MoneyTypeHandler.java
@@ -1,4 +1,4 @@
-package geektime.spring.data.mybatis.handler;
+package com.clay.spring.data.mybatis.handler;
 
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;

ab1133895640fb6f417c7d90630cdc2de2159fc8
yuntzhao <yuntzhao@ebay.com>
2019-09-24 16:59:14 +0800

    optimize springbucks

--- springbucks/src/main/java/com/clay/spring/data/springbucks/SpringbucksApplication.java
+++ springbucks/src/main/java/com/clay/spring/data/springbucks/SpringbucksApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -33,6 +34,7 @@ import java.util.Optional;
 @Slf4j
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableCaching(proxyTargetClass = true)
 public class SpringbucksApplication implements ApplicationRunner {
 
     @Autowired
@@ -68,18 +70,29 @@ public class SpringbucksApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        log.info(jedisPoolConfig.toString());
+        log.info("Count: {}", coffeeService.findAllCoffee().size());
 
-        try (Jedis jedis = jedisPool.getResource()) {
-            coffeeRepository.findAll().forEach(coffee -> {
-                jedis.hset("springbucks-menu", coffee.getName(), Long.toString(coffee.getPrice().getAmountMinorLong()));
-            });
-
-            Map<String, String> menu = jedis.hgetAll("springbucks-menu");
-            log.info("Menu: {}", menu);
-            String price = jedis.hget("springbucks-menu", "espresso");
-            log.info("espresso - {}", Money.ofMinor(CurrencyUnit.of("CNY"), Long.parseLong(price)));
+        for (int i = 0; i < 10; i++) {
+            log.info("Reading from cache.");
+            coffeeService.findAllCoffee();
         }
+
+        coffeeService.reloadCoffee();
+        log.info("Reading after refresh.");
+        coffeeService.findAllCoffee().forEach(coffee -> log.info("Coffee: {}", coffee));
+
+//        log.info(jedisPoolConfig.toString());
+//
+//        try (Jedis jedis = jedisPool.getResource()) {
+//            coffeeRepository.findAll().forEach(coffee -> {
+//                jedis.hset("springbucks-menu", coffee.getName(), Long.toString(coffee.getPrice().getAmountMinorLong()));
+//            });
+//
+//            Map<String, String> menu = jedis.hgetAll("springbucks-menu");
+//            log.info("Menu: {}", menu);
+//            String price = jedis.hget("springbucks-menu", "espresso");
+//            log.info("espresso - {}", Money.ofMinor(CurrencyUnit.of("CNY"), Long.parseLong(price)));
+//        }
 
 //        log.info("All Coffee: {}", coffeeRepository.findAll());
 //
--- springbucks/src/main/java/com/clay/spring/data/springbucks/service/CoffeeService.java
+++ springbucks/src/main/java/com/clay/spring/data/springbucks/service/CoffeeService.java
@@ -4,10 +4,14 @@ import com.clay.spring.data.springbucks.model.Coffee;
 import com.clay.spring.data.springbucks.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.exact;
@@ -17,9 +21,20 @@ import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatc
  */
 @Slf4j
 @Service
+@CacheConfig(cacheNames = "coffee")
 public class CoffeeService {
     @Autowired
     private CoffeeRepository coffeeRepository;
+
+    @Cacheable
+    public List<Coffee> findAllCoffee() {
+        return coffeeRepository.findAll();
+    }
+
+    @CacheEvict
+    public void reloadCoffee() {
+
+    }
 
     public Optional<Coffee> findOneCoffee(String name) {
         ExampleMatcher matcher = ExampleMatcher.matching()

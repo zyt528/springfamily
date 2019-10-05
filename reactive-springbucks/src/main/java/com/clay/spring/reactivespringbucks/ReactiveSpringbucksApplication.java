3bbe22edb7873796171d5936984bbb07a0f06149
yuntzhao <yuntzhao@ebay.com>
2019-10-05 23:35:02 +0800

    add project reactive-springbucks

---
+++ reactive-springbucks/src/main/java/com/clay/spring/reactivespringbucks/ReactiveSpringbucksApplication.java
@@ -0,0 +1,13 @@
+package com.clay.spring.reactivespringbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringbucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringbucksApplication.class, args);
+    }
+
+}
---
+++ reactive-springbucks/src/test/java/com/clay/spring/reactivespringbucks/ReactiveSpringbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay.spring.reactivespringbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringbucksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
